26bba075ba2e9cef874b4b575b878a91cb400f76
Daniela GC <nanisha10@gmail.com>
2022-03-27 13:52:21 -0600

    changes to project

--- project_app/app.py
+++ project_app/app.py
@@ -158,27 +158,27 @@ def main():
 
     days_to_be_rented = st.sidebar.slider('Days in a Year Expected to Rent', 1,365, 1 )
     if days_to_be_rented:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     room_type_option = st.sidebar.selectbox('Room Type',('Shared Room','Private Room','Entire house'))
     if room_type_option:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     minimum_nights = st.sidebar.slider('Minimum Nights', 0,30, 1 )
     if minimum_nights:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     number_of_reviews = st.sidebar.slider('Number of reviews', 0,629, 1 )
     if number_of_reviews:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     reviews_per_month = st.sidebar.slider('Reviews per month', 0,58, 1 )
     if reviews_per_month:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     calculated_host_listings_count = st.sidebar.slider('Number of host listings', 1,327, 1 )
     if calculated_host_listings_count:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
     availability_365= st.sidebar.slider('Availability (must be greater than expected days to rent)', 0,365, 1)
     if availability_365 < days_to_be_rented:
-        st.sidebar.write("Please check that availability is greater than expected days to rent")
+        st.sidebar.warning("Please check that availability is greater than expected days to rent")
     else:
-        st.sidebar.success('Ok!')
+        st.sidebar.info('Ok!')
         
     sideb = st.sidebar
 
